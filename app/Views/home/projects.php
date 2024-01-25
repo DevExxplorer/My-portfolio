272ba1c4d2cf0b9b6675e513a64a87448d9df402
devexplorer <contact@devexplorer.fr>
2024-01-25 15:41:36 +0100

    upd section projects

--- app/Views/home/projects.php
+++ app/Views/home/projects.php
@@ -2,162 +2,167 @@
     $imgUrl = $_SERVER["REQUEST_SCHEME"] .'://'. $_SERVER["HTTP_HOST"] . '/My-portfolio';
 ?>
 <div class="home-projects">
-    <div class="home-projects__section1">
-        <div class="project project1">
-            <div class="project__bloc">
-                <div class="project__bloc__img">
-                    <img
-                        src="<?= $imgUrl; ?>/dist/images/admissions-galerie.png"
-                        alt=""
-                    >
-                </div>
-                <div class="project__bloc__text">
-                    <h3>Admissions</h3>
-                    <div class="wysiwyg">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
-                        </p>
+    <div class="container">
+        <div class="home-projects__section1">
+            <div class="project project1">
+                <div class="project__bloc">
+                    <div class="project__bloc__img">
+                        <img
+                            src="<?= $imgUrl; ?>/dist/images/admissions-galerie.png"
+                            alt=""
+                        >
                     </div>
-                    <div class="skills">
-                        <ul>
-                            <li>NuxtJs</li>
-                            <li>VueJs</li>
-                            <li>Netlify</li>
-                        </ul>
+                    <div class="project__bloc__text">
+                        <h3>Admissions</h3>
+                        <div class="wysiwyg">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
+                            </p>
+                        </div>
+                        <div class="skills">
+                            <ul>
+                                <li>NuxtJs</li>
+                                <li>VueJs</li>
+                                <li>Netlify</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="project project2">
+                <div class="project__bloc">
+                    <div class="project__bloc__img">
+                        <img
+                            src="<?= $imgUrl; ?>/dist/images/nutrilien-galerie.png"
+                            alt=""
+                        >
+                    </div>
+                    <div class="project__bloc__text">
+                        <h3>Nutrilien</h3>
+                        <div class="wysiwyg">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
+                            </p>
+                        </div>
+                        <div class="skills">
+                            <ul>
+                                <li>HTML/CSS</li>
+                                <li>Javascript</li>
+                                <li>PHP</li>
+                                <li>Wordpress</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="project project2">
-            <div class="project__bloc">
-                <div class="project__bloc__img">
-                    <img
-                        src="<?= $imgUrl; ?>/dist/images/nutrilien-galerie.png"
-                        alt=""
-                    >
-                </div>
-                <div class="project__bloc__text">
-                    <h3>Nutrilien</h3>
-                    <div class="wysiwyg">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
-                        </p>
+        <div class="home-projects__section2">
+            <div class="project project3">
+                <div class="project__bloc">
+                    <div class="project__bloc__img">
+                        <img
+                            src="<?= $imgUrl; ?>/dist/images/viltier-galerie.png"
+                            alt=""
+                        >
                     </div>
-                    <div class="skills">
-                        <ul>
-                            <li>HTML/CSS</li>
-                            <li>Javascript</li>
-                            <li>PHP</li>
-                            <li>Wordpress</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="home-projects__section2">
-        <div class="project project3">
-            <div class="project__bloc">
-                <div class="project__bloc__img">
-                    <img
-                        src="<?= $imgUrl; ?>/dist/images/viltier-galerie.png"
-                        alt=""
-                    >
-                </div>
-                <div class="project__bloc__text">
-                    <h3>Viltier</h3>
-                    <div class="wysiwyg">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
-                        </p>
-                    </div>
-                    <div class="skills">
-                        <ul>
-                            <li>HTML/CSS</li>
-                            <li>Javascript</li>
-                            <li>PHP</li>
-                            <li>Wordpress</li>
-                        </ul>
+                    <div class="project__bloc__text">
+                        <h3>Viltier</h3>
+                        <div class="wysiwyg">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
+                            </p>
+                        </div>
+                        <div class="skills">
+                            <ul>
+                                <li>HTML/CSS</li>
+                                <li>Javascript</li>
+                                <li>PHP</li>
+                                <li>Wordpress</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="home-projects__section3">
-        <div class="project project4">
-            <div class="project__bloc">
-                <div class="project__bloc__img">
-                    <img
-                        src="<?= $imgUrl; ?>/dist/images/packshot-galerie.png"
-                        alt=""
-                    >
-                </div>
-                <div class="project__bloc__text">
-                    <h3>Packshot Creator</h3>
-                    <div class="wysiwyg">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
-                        </p>
+        <div class="home-projects__section3">
+            <div class="project project4">
+                <div class="project__bloc">
+                    <div class="project__bloc__img">
+                        <img
+                            src="<?= $imgUrl; ?>/dist/images/packshot-galerie.png"
+                            alt=""
+                        >
                     </div>
-                    <div class="skills">
-                        <ul>
-                            <li>NuxtJs</li>
-                            <li>VueJs</li>
-                            <li>Netlify</li>
-                        </ul>
+                    <div class="project__bloc__text">
+                        <h3>Packshot Creator</h3>
+                        <div class="wysiwyg">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
+                            </p>
+                        </div>
+                        <div class="skills">
+                            <ul>
+                                <li>NuxtJs</li>
+                                <li>VueJs</li>
+                                <li>Netlify</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="project project5">
+                <div class="project__bloc">
+                    <div class="project__bloc__img">
+                        <img
+                            src="<?= $imgUrl; ?>/dist/images/admissions-galerie.png"
+                            alt=""
+                        >
+                    </div>
+                    <div class="project__bloc__text">
+                        <h3>Admissions</h3>
+                        <div class="wysiwyg">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
+                            </p>
+                        </div>
+                        <div class="skills">
+                            <ul>
+                                <li>NuxtJs</li>
+                                <li>VueJs</li>
+                                <li>Netlify</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="project project6">
+                <div class="project__bloc">
+                    <div class="project__bloc__img">
+                        <img
+                            src="<?= $imgUrl; ?>/dist/images/admissions-galerie.png"
+                            alt=""
+                        >
+                    </div>
+                    <div class="project__bloc__text">
+                        <h3>Admissions</h3>
+                        <div class="wysiwyg">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
+                            </p>
+                        </div>
+                        <div class="skills">
+                            <ul>
+                                <li>NuxtJs</li>
+                                <li>VueJs</li>
+                                <li>Netlify</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="project project5">
-            <div class="project__bloc">
-                <div class="project__bloc__img">
-                    <img
-                        src="<?= $imgUrl; ?>/dist/images/admissions-galerie.png"
-                        alt=""
-                    >
-                </div>
-                <div class="project__bloc__text">
-                    <h3>Admissions</h3>
-                    <div class="wysiwyg">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
-                        </p>
-                    </div>
-                    <div class="skills">
-                        <ul>
-                            <li>NuxtJs</li>
-                            <li>VueJs</li>
-                            <li>Netlify</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="project project6">
-            <div class="project__bloc">
-                <div class="project__bloc__img">
-                    <img
-                        src="<?= $imgUrl; ?>/dist/images/admissions-galerie.png"
-                        alt=""
-                    >
-                </div>
-                <div class="project__bloc__text">
-                    <h3>Admissions</h3>
-                    <div class="wysiwyg">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet sodales erat. Aliquam erat volutpat. Aliquam a justo ante. Aliquam erat volutpat. Vestibulum ac feugiat velit. Donec vehicula lectus id neque vehicula laoreet. Nam non tincidunt nunc. Nulla facilisi. Mauris mollis suscipit dui et aliquam. Morbi eleifend porta purus, non pretium leo fringilla eget.
-                        </p>
-                    </div>
-                    <div class="skills">
-                        <ul>
-                            <li>NuxtJs</li>
-                            <li>VueJs</li>
-                            <li>Netlify</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+        <div class="home-projects__title">
+            <span>Projets</span>
         </div>
     </div>
 </div>
